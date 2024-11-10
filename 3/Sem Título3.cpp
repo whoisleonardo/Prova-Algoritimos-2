27223b25295e97357322a887828403d4a797c391
leo <leonardomdubena@gmail.com>
2024-11-10 18:36:40 -0300

    att 3

--- 3/Sem Título3.cpp
+++ 3/Sem Título3.cpp
@@ -10,8 +10,8 @@ int main(){
 	
 	system("color 0C");
 	
-	int op, i, qntb;
-	float percentb, mid, idade, somaid;
+	int op, i, qntb=0;
+	float percentb=0, mid=0, idade, somaid=0;
 	
 	for(i=0;i<15;i++){
 	printf("\nQual sua idade? ");
@@ -21,15 +21,17 @@ int main(){
 	
 	switch(op){
 		case 3:
-		somaid+=idade;
-		mid=somaid/15;
+			somaid += idade;
 		break;
 		case 2:
-		qntb++;
-		percentb=(qntb/15)*100;
+			qntb++;
 		break;
 	}
 	}
+	if(qntb > 0){
+		mid= somaid/15;
+	}
+	percentb = ((float)qntb / 15)*100;
 		printf("\nA média de idade de pessoas que responderam ótima é: %.2f", mid);
-		printf("\nO percentual de pessoas que consideram o filme bom é de: %.2f", percentb);
+		printf("\nO percentual de pessoas que consideram o filme bom é de: %.2f%%", percentb);
 }
